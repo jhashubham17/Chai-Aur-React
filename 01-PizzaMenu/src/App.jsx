70dd6a6f2df441b6709a4bf5fabcfd6898a9839c
Shubham jha <sjha870054@gmail.com>
2025-01-05 08:57:26 +0530

    Project-02

--- 01-PizzaMenu/src/App.jsx
+++ 01-PizzaMenu/src/App.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import './App.css'
+function App() {
+  return (
+    <>
 
-function App(){
-  return(
-  <h1>Shubham jha</h1>
+      <h1>Shubham jha</h1>
+    </>
+
 
   )
 }
